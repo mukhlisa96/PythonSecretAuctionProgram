6765ec34b03d59dd0d6cc6b7bf74ae30ccffc526
mukhlisa96 <mukhlisakhonkhusanboyeva@gmail.com>
2026-01-08 22:54:31 +0800

    commit

---
+++ art.py
@@ -0,0 +1,16 @@
+logo = '''
+                                                            
+                                              88  
+                                              88  
+                                              88  
+ ,adPPYb,d8 ,adPPYYba, 8b       d8  ,adPPYba, 88  
+a8"    `Y88 ""     `Y8 `8b     d8' a8P_____88 88  
+8b       88 ,adPPPPP88  `8b   d8'  8PP""""""" 88  
+"8a,   ,d88 88,    ,88   `8b,d8'   "8b,   ,aa 88  
+ `"YbbdP"Y8 `"8bbdP"Y8     "8"      `"Ybbd8"' 88  
+ aa,    ,88                                       
+  "Y8bbdP"                       
+                      
+                               
+
+'''
